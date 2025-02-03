e1a159f37a64308d9a65ad447944a400c2cdd816
Jesper Kling <jesperkling87@gmail.com>
2025-02-03 17:52:08 +0100

    Added cart quantity badge in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,28 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import { useCart } from "../contexts/CartContext";
 import styles from "./Header.module.css";
 
 function Header() {
+  const { cart } = useCart();
+
+  const totalQuantity = cart.reduce((sum, item) => sum + item.quantity, 0);
+
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
-        <Link to="/" className={styles.link}>
-          Home
-        </Link>
-        <Link to="/cart" className={styles.link}>
-          Cart
-        </Link>
+        <div className={styles.navLinks}>
+          <Link to="/" className={styles.link}>
+            Home
+          </Link>
+        </div>
+        <div className={styles.cartContainer}>
+          <Link to="/cart" className={styles.link}>
+            Cart
+          </Link>
+          {totalQuantity > 0 && (
+            <span className={styles.cartBadge}>{totalQuantity}</span>
+          )}
+        </div>
       </nav>
     </header>
   );
