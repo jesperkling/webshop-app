371c88ececa71463d850dd28a07a43b2bc77b8c8
Jesper Kling <jesperkling87@gmail.com>
2025-02-10 20:49:10 +0100

    Added global base style in index.css

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./styles/global.css";
 import App from "./App.jsx";
 import { CartProvider } from "./contexts/CartContext.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
